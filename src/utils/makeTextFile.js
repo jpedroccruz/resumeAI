d2837d24cf8e36d06c2b4b5554fec689ab3f0a38
jpedroccruz <joaopedroccruz.dev@gmail.com>
2026-05-15 19:08:52 -0300

    create makeTextFile for generateWordcloud to cosume text files

--- src/utils/makeTextFile.js
+++ src/utils/makeTextFile.js
@@ -1,14 +1,5 @@
 import { writeFile } from 'fs/promises'
 
-export default async function makeTextFile(transcription) {
-  let text = ''
-
-  transcription.captions.forEach(element => {
-    if (element.text.length > 5 && element.text != '[Música]') {
-      text += `${element.text} `
-    }
-  })
-  
-  await writeFile('./src/utils/text_files/text.txt', text, 'utf-8')
-  return text
+export default async function makeTextFile(text, filename) {
+  await writeFile(`./src/utils/text_files/${filename}.txt`, text, 'utf-8')
 }
