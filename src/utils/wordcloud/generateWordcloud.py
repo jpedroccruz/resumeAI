9ac12f4f46f1c7111b9a75797ea543c0dc5dda4b
jpedroccruz <joaopedroccruz.dev@gmail.com>
2026-05-15 19:24:12 -0300

    update generateWordcloud script to adapt to new inputs

--- src/utils/wordcloud/generateWordcloud.py
+++ src/utils/wordcloud/generateWordcloud.py
@@ -1,19 +1,17 @@
+import json
 from wordcloud import WordCloud
+from os import remove
 import matplotlib.pyplot as plt
+import json
 
-# Abre e pega o conteúdo dos arquivos de texto
 stopwords = open('./src/utils/text_files/stopwords.txt', mode='r', encoding='utf-8').read().splitlines()
-separatedTalk = open('./src/utils/text_files/separatedTalk.txt', mode='r', encoding='utf-8').read().split('\n\n') 
-text = open('./src/utils/text_files/text.txt', mode='r', encoding='utf-8').read()
+separatedTalk = json.loads(open('./src/utils/text_files/separatedTalk.txt', mode='r', encoding='utf-8').read())
+transcription = open('./src/utils/text_files/transcription.txt', mode='r', encoding='utf-8').read()
 
-# Insere o texto separado em variáveis diferentes para cada candidatos
-lodovico = separatedTalk[0]
-adriana = separatedTalk[1]
+separatedTalk = json.loads(separatedTalk)
 
-# Configuração do tamanho da imagem
 plt.rcParams["figure.figsize"] = (10, 5)
 
-# Configuração do Wordcloud
 wordcloud = WordCloud(
   max_font_size=70,
   max_words=75,
@@ -22,7 +20,6 @@ wordcloud = WordCloud(
   colormap='flag'
 )
 
-# Função que cria os Wordclouds
 def createWordcloud(words, fileName):
   wordcloud.generate(words)
 
@@ -32,7 +29,9 @@ def createWordcloud(words, fileName):
   plt.savefig(f'./src/utils/wordcloud/_{fileName}')
 
 if __name__ == '__main__':
-  # Cria os 3 wordclouds
-  createWordcloud(text, 'wordcloud')
-  createWordcloud(lodovico, 'lodovico')
-  createWordcloud(adriana, 'adriana')
+  createWordcloud(transcription, 'wordcloud')
+
+  for character in separatedTalk:
+    createWordcloud(character["talk"], character["character"])
+  
+  remove('./src/utils/wordcloud/*.png')
